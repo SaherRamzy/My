1f71540995a4156cf424c34d2cf0da50a65f677e
saherramzy <eng.saher9@gmail.com>
2015-12-09 22:20:08 +0200

    check push 2

--- app/src/main/java/com/example/saher/myapplication/MainActivity.java
+++ app/src/main/java/com/example/saher/myapplication/MainActivity.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+    TextView tv2;
+    Button button;
+    EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String sd = "sd";
-        String df="df";
-        finish();
+        et1 = (EditText) findViewById(R.id.et_type);
+        tv2 = (TextView) findViewById(R.id.tv_show);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type =et1.getText().toString();
+                tv2.setText(type);
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
