97f0498de2f9fad488f745a81f5074352481095b
saherramzy <eng.saher9@gmail.com>
2015-12-09 16:46:32 +0200

    check push 1

--- app/src/main/java/com/example/saher/myapplication/MainActivity.java
+++ app/src/main/java/com/example/saher/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String sd = "sd";
+        String df="df";
         finish();
     }
 
