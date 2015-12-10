b371b6fb8c3393fec28b682c6314cf9e596fdd8a
saherramzy <eng.saher9@gmail.com>
2015-12-10 22:08:38 +0200

    check push 3

--- app/src/main/java/com/example/saher/myapplication/MainActivity.java
+++ app/src/main/java/com/example/saher/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.saher.myapplication;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,48 +11,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+
 public class MainActivity extends ActionBarActivity {
-    TextView tv2;
-    Button button;
-    EditText et1;
+//    TextView tv2;
+//    Button button;
+//    EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et1 = (EditText) findViewById(R.id.et_type);
-        tv2 = (TextView) findViewById(R.id.tv_show);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String type =et1.getText().toString();
-                tv2.setText(type);
-            }
-        });
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//        et1 = (EditText) findViewById(R.id.et_type);
+//        tv2 = (TextView) findViewById(R.id.tv_show);
+//        button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String type =et1.getText().toString();
+//                tv2.setText(type);
+//            }
+//        });
+         }
+    public void click(View view) {
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
